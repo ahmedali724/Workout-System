d4b08a56ee508f5516319af9b6e697de369e2644
YSFAHM <ysfahm1@outlook.com>
2025-12-28 13:38:47 +0200

    Fix Template Class With cpp and hpp files

--- Utils/Database.hpp
+++ Utils/Database.hpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <functional>
+#include <type_traits>
 #include "sqlite3.h"
 
 class DB
@@ -10,17 +11,58 @@ private:
     sqlite3* db;
 
 public:
-    DB(const std::string& file);
-    ~DB();
+    DB(const std::string& file)
+    {
+        sqlite3_open(file.c_str(), &db);
+    }
 
-    bool exec(const std::string& sql);
+    ~DB()
+    {
+        sqlite3_close(db);
+    }
+
+    bool exec(const std::string& sql)
+    {
+        return sqlite3_exec(db, sql.c_str(), nullptr, nullptr, nullptr) == SQLITE_OK;
+    }
 
     template<typename T>
     std::vector<T> query(
         const std::string& sql,
-        std::function<T(sqlite3_stmt*)> mapper
-    );
+        std::function<T(sqlite3_stmt*)> mapper)
+    {
+        std::vector<T> result;
+        sqlite3_stmt* stmt;
+
+        if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK)
+            return result;
+
+        while (sqlite3_step(stmt) == SQLITE_ROW)
+            result.push_back(mapper(stmt));
+
+        sqlite3_finalize(stmt);
+        return result;
+    }
 
     template<typename T>
-    T scalar(const std::string& sql);
+    T scalar(const std::string& sql)
+    {
+        sqlite3_stmt* stmt;
+        T value{};
+
+        if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) == SQLITE_OK)
+        {
+            if (sqlite3_step(stmt) == SQLITE_ROW)
+            {
+                if constexpr (std::is_same_v<T, int>)
+                    value = sqlite3_column_int(stmt, 0);
+                else if constexpr (std::is_same_v<T, double>)
+                    value = sqlite3_column_double(stmt, 0);
+                else if constexpr (std::is_same_v<T, std::string>)
+                    value = (const char*)sqlite3_column_text(stmt, 0);
+            }
+        }
+        sqlite3_finalize(stmt);
+        return value;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -8,57 +8,60 @@ using namespace tui;
 
 /* ================= MAIN ================= */
 
-int main()
-{
-    system("cls");
+// int main()
+// {
+//     system("cls");
 
-    printAt(20, 1, "=== CONSOLE UI SYSTEM ===", 11);
+//     printAt(20, 1, "=== CONSOLE UI SYSTEM ===", 11);
 
-    /* ----- Single Text Field ----- */
-    printAt(5, 3, "ID:");
-    vector<char> idVec = addTextField(15, 3, 10, '0', '9');
-    drawHLine(20,20,30);
-    /* ----- MULTI TEXT FIELD ----- */
-    printAt(5, 5, "Name:");
-    printAt(5, 7, "Age:");
-    printAt(5, 9, "City:");
+//     /* ----- Single Text Field ----- */
+//     printAt(5, 3, "ID:");
+//     vector<char> idVec = addTextField(15, 3, 10, '0', '9');
+//     drawHLine(20,20,30);
+//     /* ----- MULTI TEXT FIELD ----- */
+//     printAt(5, 5, "Name:");
+//     printAt(5, 7, "Age:");
+//     printAt(5, 9, "City:");
 
-    vector<int> xPos = {15, 50, 15};
-    vector<int> yPos = {5, 5, 9};
-    vector<int> length = {20, 3, 15};
-    vector<string> data = {"1", "2", "3"};
+//     vector<int> xPos = {15, 50, 15};
+//     vector<int> yPos = {5, 5, 9};
+//     vector<int> length = {20, 3, 15};
+//     vector<string> data = {"1", "2", "3"};
 
-    vector<char> sRange = {'A', '0', 'A'};
-    vector<char> eRange = {'z', '9', 'z'};
+//     vector<char> sRange = {'A', '0', 'A'};
+//     vector<char> eRange = {'z', '9', 'z'};
 
-    vector<string> dataVec =
-        addMultiTextField(xPos, yPos, length, 3, sRange, eRange, data);
+//     vector<string> dataVec =
+//         addMultiTextField(xPos, yPos, length, 3, sRange, eRange, data);
 
-    /* ----- Menu ----- */
-    vector<string> menu = {
-        "Save",
-        "Edit",
-        "Exit"};
+//     /* ----- Menu ----- */
+//     vector<string> menu = {
+//         "Save",
+//         "Edit",
+//         "Exit"};
 
-    int choice = showMenu(5, 12, menu);
+//     int choice = showMenu(5, 12, menu);
 
-    system("cls");
+//     system("cls");
 
-    // convert vectors to strings for display
-    string id(idVec.begin(), idVec.end());
-    string name = dataVec[0];
-    string age = dataVec[1];
-    string city = dataVec[2];
+//     // convert vectors to strings for display
+//     string id(idVec.begin(), idVec.end());
+//     string name = dataVec[0];
+//     string age = dataVec[1];
+//     string city = dataVec[2];
 
-    printAt(5, 3, "ID   : " + id);
-    printAt(5, 4, "Name : " + name);
-    printAt(5, 5, "Age  : " + age);
-    printAt(5, 6, "City : " + city);
-    printAt(5, 8, "Menu : " + menu[choice], 10);
+//     printAt(5, 3, "ID   : " + id);
+//     printAt(5, 4, "Name : " + name);
+//     printAt(5, 5, "Age  : " + age);
+//     printAt(5, 6, "City : " + city);
+//     printAt(5, 8, "Menu : " + menu[choice], 10);
 
-    getch();
-    return 0;
-}
+//     getch();
+//     return 0;
+// }
+
+
+
 
 
 
